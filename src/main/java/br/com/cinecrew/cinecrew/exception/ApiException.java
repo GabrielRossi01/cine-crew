74e03109cfbf3b99709a267f07662aab9c10de74
GabrielRossi01 <rossi17006@gmail.com>
2026-08-26 11:38:54 -0300

    adiciona anotação Getter em ApiException para método getStatus() funcionar em GlobalExceptionHandler

--- src/main/java/br/com/cinecrew/cinecrew/exception/ApiException.java
+++ src/main/java/br/com/cinecrew/cinecrew/exception/ApiException.java
@@ -1,7 +1,9 @@
 package br.com.cinecrew.cinecrew.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public abstract class ApiException extends RuntimeException {
 
     private final HttpStatus status;
